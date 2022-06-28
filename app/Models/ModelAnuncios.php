dbb093ea2ee418390582c0b4ccc36f069625cf7c
Eduardo Manoel de Lara <52054453+Eduardo2712@users.noreply.github.com>
2022-06-27 22:23:27 -0300

    Rota anúncio id

--- app/Models/ModelAnuncios.php
+++ app/Models/ModelAnuncios.php
@@ -28,11 +28,15 @@ class ModelAnuncios extends Model
                 "sucesso" => false,
             ], 400);
         }
-        $anuncios = DB::table(DB::raw("anuncios as a"))->select("a.id", "a.id_usuario", "a.titulo", "a.descricao", "a.ativo", "a.data_criado", "a.data_atualizado", "a.id_tipo_anuncio", "a.bairro", "a.cep", "a.complemento", "a.numero", "a.rua", "a.cidade", "a.estado")->where("a.id", "=", $id)->first();
-        // $anuncios["tipo_anuncios"] = DB::table(DB::raw("tipo_anuncios as t"))->select("t.id", "t.nome")->where("t.id", "=", $id)->first();
+        $anuncio["anuncio"] = DB::table(DB::raw("anuncios as a"))->select("a.id", "a.id_usuario", "a.titulo", "a.descricao", "a.ativo", "a.data_criado", "a.data_atualizado", "a.id_tipo_anuncio", "a.bairro", "a.cep", "a.complemento", "a.numero", "a.rua", "a.cidade", "a.estado")->where("a.id", "=", $id)->first();
+        $anuncio["tipo_anuncios"] = DB::table(DB::raw("tipo_anuncios as t"))->select("t.id", "t.nome")->where("t.id", "=", $id)->first();
+        $anuncio["usuarios"] = DB::table(DB::raw("usuarios as u"))->select("u.id", "u.nome", "u.email", "u.telefone")->where("u.id", "=", $id)->first();
+        $anuncio["imagens"] = DB::table(DB::raw("imagens as i"))->select("i.id", "i.nome", "i.caminho", "i.id_anuncio")->where("i.ativo", "=", 1)->where("i.id_anuncio", "=", $id)->get();
+        $anuncio["comentarios"] = DB::table(DB::raw("comentarios as c"))->select("c.id", "c.comentario", "c.id_usuario", "c.id_anuncio")->where("c.id_anuncio", "=", $id)->get();
+        $anuncio["valores"] = DB::table(DB::raw("valores as v"))->select("v.id", "v.valor", "v.data_criado", "v.ativo", "v.id_anuncio")->where("v.id_anuncio", "=", $id)->get();
         return response()->json([
             "mensagem" => "",
-            "anuncios" => $anuncios,
+            "anuncios" => $anuncio,
             "sucesso" => true,
         ], 200);
     }
